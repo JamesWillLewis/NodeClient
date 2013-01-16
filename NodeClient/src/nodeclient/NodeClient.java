39a66c77ab3200e64d08d47690413b228588aa6f
James <James@James-PC>
2013-01-16 12:34:46 +0200

    Created repo.

---
+++ NodeClient/src/nodeclient/NodeClient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodeclient;
+
+/**
+ *
+ * @author James
+ */
+public class NodeClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
